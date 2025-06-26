0b1c803eb5140fdb7019c0bbda3b7b4d9c6b1fcc
Kaushalendra Singh <yadavkausha4a5@gmail.com>
2025-06-26 18:01:30 +0530

    Update ThreadCard.tsx Timing feature

--- components/cards/ThreadCard.tsx
+++ components/cards/ThreadCard.tsx
@@ -65,6 +65,9 @@ const ThreadCard = ({
                         <p className="mt-2 text-small-regular text-light-2">
                             {content}
                         </p>
+                        <p className="text-[12px] text-gray-1">
+                            {formatDateString(createdAt)}
+                        </p>
                         <div className="flex flex-col gap-3 mt-5">
                             <div className="flex gap-4">
                                 <Image src="/assets/heart-gray.svg" alt="heart" width={24} height={24} className="object-contain cursor-pointer" />
