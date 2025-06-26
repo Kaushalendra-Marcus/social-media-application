a3e933933d5b9778619767340a425fc8546cb8d1
Kaushalendra Singh <yadavkausha4a5@gmail.com>
2025-06-26 17:14:55 +0530

    Update botttombar

--- components/shared/Bottombar.tsx
+++ components/shared/Bottombar.tsx
@@ -1,5 +1,6 @@
 "use client"
 import { sidebarLinks } from '@/constants'
+import { useAuth } from '@clerk/nextjs';
 import Image from 'next/image';
 import Link from 'next/link';
 import { usePathname, useRouter } from 'next/navigation'
@@ -8,29 +9,37 @@ import React from 'react'
 const Bottombar = () => {
   const pathname = usePathname();
   const router = useRouter();
+  const { userId } = useAuth();  // ✅ Correct destructuring
+
   return (
     <section className='bottombar'>
       <div className='bottombar_container'>
         {sidebarLinks.map((link) => {
-          const isActive = (pathname.includes(link.route) && link.route.length > 1) || pathname === link.route;
+          let linkRoute = link.route;
+          if (link.route === '/profile') {
+            linkRoute = `${link.route}/${userId}`;
+          }
+
+          const isActive =
+            (pathname.includes(linkRoute) && linkRoute.length > 1) ||
+            pathname === linkRoute;
 
           return (
             <Link
-              href={link.route}
+              href={linkRoute}
               key={link.label}
-              className={`bottombar_link ${isActive && "bg-primary-500"}`}
+              className={`bottombar_link ${isActive ? "bg-primary-500" : ""}`}
             >
               <Image src={link.imgURL} alt={link.label} width={24} height={24} />
               <p className='text-light-1 max-sm:hidden'>
                 {link.label.split(/\s+/)[0]}
               </p>
             </Link>
-          )
-        }
-        )}
+          );
+        })}
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Bottombar
+export default Bottombar;
